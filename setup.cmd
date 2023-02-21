67056cfbe734ca432542eee9202830de03963430
nikoiss <28nszczypinski@isstavanger.no>
2023-02-21 12:24:08 +0100

    fixes

--- setup.cmd
+++
@@ -1,7 +0,0 @@
-echo "# sensorywall" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/niko-iss/sensorywall.git
-git push -u origin main
